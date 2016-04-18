321276e9246e5d569d58d08ea8b63b39cda382aa
scal1029 <scal1029@hs-karlsruhe.de>
2016-04-18 09:57:40 +0200

    Add project files.

---
+++ ProBikeSS16/Data Binding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ProBikeSS16.Annotations;
+
+namespace ProBikeSS16
+{
+    public class PageBinding : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string TextBinding
+        {
+            get { return _TextBinding; }
+            set
+            {
+                _TextBinding = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("TextBinding"));
+                }
+            }
+        }
+
+        private string _TextBinding = "<empty>";
+
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ ProBikeSS16/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProBikeSS16;
+using Xceed.Wpf.Toolkit;
+
+namespace ProBikeSS16
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (MenuItem item in menuItemLanguages.Items)
+            {
+                if (item.Tag.ToString().Equals(CultureInfo.CurrentUICulture.Name))
+                    item.IsChecked = true;
+            }
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void LanguageHeader_Click(object sender, RoutedEventArgs e)
+        {
+            // Uncheck each item
+            foreach (MenuItem item in menuItemLanguages.Items)
+            {
+                item.IsChecked = false;
+            }
+
+            MenuItem mi = sender as MenuItem;
+            mi.IsChecked = true;
+            App.Instance.SwitchLanguage(mi.Tag.ToString());        
+        }
+
+        public IntegerUpDown SafetyStockChildBike1
+        {
+            get { return SafetyStockChildBike; }
+            set { SafetyStockChildBike = value; }
+        }
+    }
+}
