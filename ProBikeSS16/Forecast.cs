50837e0353e577d206df8717c16b10d394f489c8
scal1029 <scal1029@hs-karlsruhe.de>
2016-04-18 12:45:54 +0200

    test

--- ProBikeSS16/Forecast.cs
+++ ProBikeSS16/Forecast.cs
@@ -11,5 +11,7 @@ namespace ProBikeSS16
     class Forecast
     {
 
+         
+  
     }
 }
--- ProBikeSS16/MainWindow.xaml.cs
+++ ProBikeSS16/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ProBikeSS16;
 using Xceed.Wpf.Toolkit;
+using MessageBox = System.Windows.MessageBox;
 
 namespace ProBikeSS16
 {
@@ -51,10 +52,54 @@ namespace ProBikeSS16
             App.Instance.SwitchLanguage(mi.Tag.ToString());        
         }
 
-        public IntegerUpDown SafetyStockChildBike1
+        #region Save ForecastAndSales
+        public int? StockChildBike;
+        public int? StockMaleBike;
+        public int? StockFemaleBike;
+        public int? SaleChildBikeN;
+        public int? SaleChildBikeN1;
+        public int? SaleChildBikeN2;
+        public int? SaleChildBikeN3;
+        public int? SaleFemaleBikeN;
+        public int? SaleFemaleBikeN1;
+        public int? SaleFemaleBikeN2;
+        public int? SaleFemaleBikeN3;
+        public int? SaleMaleBikeN;
+        public int? SaleMaleBikeN1;
+        public int? SaleMaleBikeN2;
+        public int? SaleMaleBikeN3;
+
+        private void SaveSafetyStockAndSales_OnClick(object sender, RoutedEventArgs e)
         {
-            get { return SafetyStockChildBike; }
-            set { SafetyStockChildBike = value; }
+            if (!SafetyStockChildBike.Value.HasValue || !SafetyStockFemaleBike.Value.HasValue || !SafetyStockMaleBike.Value.HasValue || !SalesChildBikeN.Value.HasValue ||
+                !SalesChildBikeN1.Value.HasValue || !SalesChildBikeN2.Value.HasValue || !SalesChildBikeN3.Value.HasValue || !SalesFemaleBikeN.Value.HasValue ||
+                !SalesFemaleBikeN1.Value.HasValue || !SalesFemaleBikeN2.Value.HasValue || !SalesFemaleBikeN3.Value.HasValue || !SalesMaleBikeN.Value.HasValue ||
+                !SalesMaleBikeN1.Value.HasValue || !SalesMaleBikeN2.Value.HasValue || !SalesMaleBikeN3.Value.HasValue)
+            {
+                MessageBox.Show("Error");
+            }
+            else
+            {
+                StockChildBike = SafetyStockChildBike.Value;
+                StockFemaleBike = SafetyStockFemaleBike.Value;
+                StockMaleBike = SafetyStockMaleBike.Value;
+                SaleChildBikeN = SalesChildBikeN.Value;
+                SaleChildBikeN1 = SalesChildBikeN1.Value;
+                SaleChildBikeN2 = SalesChildBikeN2.Value;
+                SaleChildBikeN3 = SalesChildBikeN3.Value;
+                SaleFemaleBikeN = SalesFemaleBikeN.Value;
+                SaleFemaleBikeN1 = SalesFemaleBikeN1.Value;
+                SaleFemaleBikeN2 = SalesFemaleBikeN2.Value;
+                SaleFemaleBikeN3 = SalesFemaleBikeN3.Value;
+                SaleMaleBikeN = SalesMaleBikeN.Value;
+                SaleMaleBikeN1 = SalesMaleBikeN1.Value;
+                SaleMaleBikeN2 = SalesMaleBikeN2.Value;
+                SaleMaleBikeN3 = SalesMaleBikeN3.Value;
+                MessageBox.Show(StockChildBike.Value.ToString()+" Success");
+                
+            }
+
         }
+        #endregion 
     }
 }
