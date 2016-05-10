31a0d4c1a456177a94a64a5d8f30039031a8f55c
Luke <Luke@LUKE-THINKPAD>
2016-05-10 15:54:23 +0200

    Domain Klassen Global und Unit

---
+++ ProBikeSS16/domain/Global.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBikeSS16.domain
+{
+    class Global
+    {
+        // Fertigteile anlegen
+        Unit e04 = new Unit(4, type.s);
+        Unit e05 = new Unit(5, type.s);
+        Unit e06 = new Unit(6, type.s);
+        Unit e07 = new Unit(7, type.s);
+        Unit e08 = new Unit(8, type.s);
+        Unit e09 = new Unit(9, type.s);
+        Unit e10 = new Unit(10, type.s);
+        Unit e11 = new Unit(11, type.s);
+        Unit e12 = new Unit(12, type.s);
+        Unit e13 = new Unit(13, type.s);
+        Unit e14 = new Unit(14, type.s);
+        Unit e15 = new Unit(15, type.s);
+        Unit e16 = new Unit(16, type.s);
+        Unit e17 = new Unit(17, type.s);
+        Unit e18 = new Unit(18, type.s);
+        Unit e19 = new Unit(19, type.s);
+        Unit e20 = new Unit(20, type.s);
+        Unit e26 = new Unit(26, type.s);
+        Unit e29 = new Unit(29, type.s);
+        Unit e30 = new Unit(30, type.s);
+        Unit e31 = new Unit(31, type.s);
+        Unit e49 = new Unit(49, type.s);
+        Unit e50 = new Unit(50, type.s);
+        Unit e51 = new Unit(51, type.s);
+        Unit e54 = new Unit(54, type.s);
+        Unit e55 = new Unit(55, type.s);
+        Unit e56 = new Unit(56, type.s);
+
+        // Kaufeile anlegen
+        Unit e21 = new Unit(21, type.b);
+        Unit e22 = new Unit(21, type.b);
+        Unit e23 = new Unit(21, type.b);
+        Unit e24 = new Unit(21, type.b);
+        Unit e25 = new Unit(21, type.b);
+        Unit e27 = new Unit(21, type.b);
+        Unit e28 = new Unit(21, type.b);
+        Unit e32 = new Unit(21, type.b);
+        Unit e33 = new Unit(21, type.b);
+        Unit e34 = new Unit(21, type.b);
+        Unit e35 = new Unit(21, type.b);
+        Unit e36 = new Unit(21, type.b);
+        Unit e37 = new Unit(21, type.b);
+        Unit e38 = new Unit(21, type.b);
+        Unit e39 = new Unit(21, type.b);
+        Unit e40 = new Unit(21, type.b);
+        Unit e41 = new Unit(21, type.b);
+        Unit e42 = new Unit(21, type.b);
+        Unit e43 = new Unit(21, type.b);
+        Unit e44 = new Unit(21, type.b);
+        Unit e45 = new Unit(21, type.b);
+        Unit e46 = new Unit(21, type.b);
+        Unit e47 = new Unit(21, type.b);
+        Unit e48 = new Unit(21, type.b);
+        Unit e52 = new Unit(21, type.b);
+        Unit e53 = new Unit(21, type.b);
+        Unit e57 = new Unit(21, type.b);
+        Unit e58 = new Unit(21, type.b);
+        Unit e59 = new Unit(21, type.b);
+    }
+}
---
+++ ProBikeSS16/domain/Unit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBikeSS16.domain
+{
+    public enum type { s, b }
+    class Unit
+    {
+        public int id;
+        public type t;
+
+        public Unit (int id, type t)
+        {
+            this.id = id;
+            this.t = t;
+        }
+
+  }
+}
