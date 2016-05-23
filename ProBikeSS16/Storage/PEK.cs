dc5a6ea0d76b144696f9a013dead96159aee8570
Jürgen <jaymessage@live.de>
2016-05-23 17:22:18 +0200

    Added Storage Classes

---
+++ ProBikeSS16/Storage/PEK.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBikeSS16
+{
+    abstract class PEK
+    {
+        protected char prefix;
+        private int quantity = 0;
+        private uint id;
+        int cost = 0;
+
+
+        public char Prefix
+        {
+            get
+            {
+                return prefix;
+            }
+        }
+
+        public int Quantity
+        {
+            get
+            {
+                return quantity;
+            }
+        }
+
+        public uint Id
+        {
+            get
+            {
+                return id;
+            }
+        }
+
+        public int Cost
+        {
+            get
+            {
+                return cost;
+            }
+        }
+
+        public PEK (uint id, int quantity = 0, int cost = 0)
+        {
+            if (id < 60)
+                throw new ArgumentOutOfRangeException();
+
+            this.id = id;
+            this.quantity = quantity;
+            this.cost = cost;
+        }
+
+        public override bool Equals(object obj)
+        {
+            PEK p = obj as PEK;
+
+            return p != null && this.prefix == p.Prefix
+                && this.id == p.Id && this.quantity == p.Quantity
+                && this.cost == p.Cost;
+        }
+
+        public override int GetHashCode()
+        {
+            return prefix.GetHashCode() ^ id.GetHashCode() 
+                ^ quantity.GetHashCode() ^ cost.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder s = new StringBuilder(Prefix);
+            
+            s.AppendLine(id.ToString());
+            s.Append("Quantity: ");
+            s.AppendLine(quantity.ToString());
+            s.Append("Costs: ");
+            s.Append(cost);
+
+            return s.ToString();
+        }
+    }
+}
---
+++ ProBikeSS16/Storage/Storage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBikeSS16
+{
+    sealed class Storage
+    {
+        static readonly Storage instance = new Storage();
+
+        HashSet<PEK> content = new HashSet<PEK>();
+
+        private Storage()
+        {
+            initStorageContent();
+        }
+
+        internal static Storage Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private void initStorageContent()
+        {
+            for (uint i = 1; i <= 3; i++)
+                content.Add(new Product(i));
+            initFabricates();
+            initPurchaseParts();
+        }
+
+        private void initFabricates()
+        {
+            for (uint i = 4; i <= 20; i++)
+                content.Add(new Fabricate(i));
+
+            content.Add(new Fabricate(26));
+            content.Add(new Fabricate(29));
+            content.Add(new Fabricate(30));
+            content.Add(new Fabricate(31));
+            content.Add(new Fabricate(49));
+            content.Add(new Fabricate(50));
+            content.Add(new Fabricate(51));
+            content.Add(new Fabricate(54));
+            content.Add(new Fabricate(55));
+            content.Add(new Fabricate(56));
+
+        }
+
+        private void initPurchaseParts()
+        {
+            for (uint i = 21; i <= 25; i++)
+                content.Add(new PurchasePart(i));
+
+            content.Add(new PurchasePart(27));
+            content.Add(new PurchasePart(28));
+
+            for (uint i = 32; i <= 48; i++)
+                content.Add(new PurchasePart(i));
+
+            content.Add(new PurchasePart(52));
+            content.Add(new PurchasePart(53));
+            content.Add(new PurchasePart(57));
+            content.Add(new PurchasePart(58));
+            content.Add(new PurchasePart(59));
+        }
+    }
+}
