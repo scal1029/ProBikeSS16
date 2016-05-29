a40a75e9bd62299f826bca815702f0a8d533e37c
scal1029 <scal1029@hs-karlsruhe.de>
2016-05-29 15:30:51 +0200

    test

---
+++ ProBikeSS16/KapazitätPrototyp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBikeSS16
+{
+    class KapazitätPrototyp
+    {
+        public void Kapazitätsplannung(List<Order> ListeGeplannteProduktion, List<KeyValuePair<int, int>>  Lagerstand )
+        {
+            
+        }
+    }
+}
---
+++ ProBikeSS16/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBikeSS16
+{
+    class Order
+    {
+        int Periode;
+        int Auftragnummer;
+        int ArtikelID;
+        int Menge;
+
+        public Order(int _Periode, int _Auftrag, int _ID, int _Menge)
+        {
+            Periode = _Periode;
+            Auftragnummer = _Auftrag;
+            ArtikelID = _ID;
+            Menge = _Menge;
+        }
+    }
+}
