6d33dfe656f0fc7511184a754322b3eb2b12fc74
jay1Zaw <jaymessage@live.de>
2016-06-18 17:39:10 +0200

    Costs Calculated at MouseOver Whole Capa

--- ProBikeSS16/Workplace.cs
+++ ProBikeSS16/Workplace.cs
@@ -63,6 +63,10 @@ namespace ProBikeSS16
             {
                 return currentWorkTime;
             }
+            set
+            {
+                currentWorkTime = value;
+            }
         }
 
         public int ShiftsToDo
@@ -74,10 +78,7 @@ namespace ProBikeSS16
 
             set
             {
-
-                if (value < 1 || value > 3)
-                    throw new ArgumentOutOfRangeException();
-                shiftsToDo = value;
+                shiftsToDo = value < 0 ? 0 : value;
             }
         }
 
@@ -187,6 +188,59 @@ namespace ProBikeSS16
             s.AppendLine(prod_batch.ToString());
 
             return s.ToString();
+        }
+
+        internal double getApproxWageCosts()
+        {
+            double sum = this.currentWorkTime;
+            double ret = 0;
+
+            if (currentWorkTime > 7200)
+                return 0;
+            if (sum > 6000 && sum <= 7200 && shiftsToDo == 3)
+            {
+                Console.WriteLine("Sum S3Ü b = " + sum);
+                ret += (sum - 4800) * 0.7;
+                sum = sum - (sum - 4800);
+                Console.WriteLine("Sum S3 = " + sum);
+            }
+            if (sum > 4800 && sum <= 6000 && shiftsToDo == 2)
+            {
+                Console.WriteLine("Sum S2Ü b = " + sum);
+                ret += (sum - 4800) * 0.9;
+                sum = sum - (sum - 4800);
+                Console.WriteLine("Sum S2Ü = " + sum);
+            }
+            if (sum > 3600 && sum <= 4800 && shiftsToDo == 2)
+            {
+                Console.WriteLine("Sum S2 b = " + sum);
+                ret += (sum - 2400) * 0.55;
+                sum = sum - (sum - 2400);
+                Console.WriteLine("Sum S2 = " + sum);
+            }
+            if (sum > 2400 && sum <= 3600 && shiftsToDo == 1)
+            {
+                Console.WriteLine("Sum S1Ü b = " + sum);
+                ret += (sum - 2400) * 0.9;
+                sum = sum - (sum - 2400);
+                Console.WriteLine("Sum S1Ü a = " + sum);
+            }
+            if (sum > 0 && sum <= 2400 && shiftsToDo == 1)
+            {
+                Console.WriteLine("Sum S1 b = " + sum);
+                ret += sum * 0.45;
+                sum -= sum;
+                Console.WriteLine("Sum S1 = " + sum);
+            }
+
+            return Math.Round(ret, 2);   
+        }
+
+        internal double getApproxMaachineCosts()
+        {
+            double costs = fix_machineCosts + var_machineCosts;
+
+            return Math.Round(currentWorkTime * costs, 2);
         }
     }
 }
